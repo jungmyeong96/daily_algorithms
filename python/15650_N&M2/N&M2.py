808e62656be784211c4b193aa5456ae6d14b9848
한중명 <gkswndaud@gmail.com>
2023-02-07 11:25:26 +0900

    01tile with dp

--- python/15650_N&M2/N&M2.py
+++ python/15650_N&M2/N&M2.py
@@ -2,20 +2,35 @@ N, M = map(int,input().split())
 
 stack = []
 
-def backTracking():
+# def backTracking():
+#     if len(stack) == M:
+#         print(" ".join(map(str, stack)))
+#         return
+#     for num in range(1, N + 1):
+#         length = len(stack)
+#         if num not in stack:
+#             if length == 0:
+#                 stack.append(num)
+#             elif num > stack[length - 1]:
+#                 stack.append(num)
+#             else :
+#                 continue
+#             backTracking()
+#             stack.pop()
+
+
+
+def backTracking(start):
     if len(stack) == M:
         print(" ".join(map(str, stack)))
         return
-    for num in range(1, N + 1):
+    for num in range(start, N + 1):
         length = len(stack)
         if num not in stack:
-            if length == 0:
-                stack.append(num)
-            elif num > stack[length - 1]:
-                stack.append(num)
-            else :
-                continue
-            backTracking()
+            stack.append(num)
+            backTracking(start + 1)
             stack.pop()
 
-backTracking()
+
+
+backTracking(1)
