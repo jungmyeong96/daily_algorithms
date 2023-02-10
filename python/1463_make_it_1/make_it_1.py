486e969054a35f307686db4487bc0b32b4be03ae
한중명 <gkswndaud@gmail.com>
2023-02-10 10:14:32 +0900

    make it 1 by top down

--- python/1463_make_it_1/make_it_1.py
+++ python/1463_make_it_1/make_it_1.py
@@ -5,13 +5,32 @@ import sys
 
 N = int(sys.stdin.readline())
 
-counts = [0] * (N + 1)
+# counts = [0] * (N + 1)
 
-for i in range(2, N + 1):
-    counts[i] = counts[i - 1] + 1
-    if i % 2 == 0:
-        counts[i] = min(counts[i], counts[i // 2] + 1)
-    if i % 3 == 0:
-        counts[i] = min(counts[i], counts[i // 3] + 1)
+# for i in range(2, N + 1):
+#     counts[i] = counts[i - 1] + 1
+#     if i % 2 == 0:
+#         counts[i] = min(counts[i], counts[i // 2] + 1)
+#     if i % 3 == 0:
+#         counts[i] = min(counts[i], counts[i // 3] + 1)
 
-sys.stdout.write("%d\n" % counts[N])
+# sys.stdout.write("%d\n" % counts[N])
+
+
+#Top down
+
+dp = {1:0}
+
+def rec(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n % 3 == 0) and (n % 2 == 0):
+        dp[n] = min(rec(n//3)+1, rec(n//2) + 1)
+    elif n%3 == 0:
+        dp[n] = min(rec(n//3) + 1, rec(n-1) + 1)
+    elif n%2 == 0:
+        dp[n] = min(rec(n//2) + 1, rec(n-1) + 1)
+    else:
+        dp[n] = rec(n-1) + 1
+    return dp[n]
+sys.stdout.write("%d\n" % rec(n))
