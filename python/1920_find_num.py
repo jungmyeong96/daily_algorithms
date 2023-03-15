bb503baf00c9b20943e5fad4d6504b58df89e142
한중명 <gkswndaud@gmail.com>
2023-03-15 12:40:53 +0900

    find_num with binary search

--- python/1920_find_num.py
+++ python/1920_find_num.py
@@ -1,12 +1,38 @@
-import sys
 
-N = sys.stdin.readline()
+#1. 자료형으로 풀기
+# import sys
 
-nNums = set(map(int, sys.stdin.readline().split()))
+# N = sys.stdin.readline()
 
-M = sys.stdin.readline()
+# nNums = set(map(int, sys.stdin.readline().split()))
 
-mNums = list(map(int, sys.stdin.readline().split()))
+# M = sys.stdin.readline()
 
-for num in mNums:
-    print(1) if num in nNums else print(0)
+# mNums = list(map(int, sys.stdin.readline().split()))
+
+# for num in mNums:
+#     print(1) if num in nNums else print(0)
+
+#2. 이분탐색으로 풀기
+from sys import stdin, stdout
+n = stdin.readline()
+nNums = sorted(list((map(int,stdin.readline().split()))))
+m = stdin.readline()
+mNums = list(map(int, stdin.readline().split()))
+print(mNums)
+
+def binary(l, nNums, start, end):
+    if start > end:
+        return 0
+    idx = (start+end)//2
+    if l == nNums[idx]:
+        return 1
+    elif l < nNums[idx]:
+        return binary(l, nNums, start, idx-1)
+    else:
+        return binary(l, nNums, idx+1, end)
+
+for l in mNums:
+    start = 0
+    end = len(nNums)-1
+    print(binary(l,nNums,start,end))
