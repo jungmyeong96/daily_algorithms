ae40b54e49e4d3f503981b7daa90008c66e2f0c0
한중명 <gkswndaud@gmail.com>
2023-04-17 12:00:51 +0900

    implements binary tree with BFS algorithm

---
+++ Leetcode/Tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,35 @@
+import collections
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+    queue = collections.deque([root])
+    depth = 0
+
+    while queue:
+        depth += 1
+        #큐 연산 추출 노드의 자식 노드 삽입
+        for _ in range(len(queue)):
+            cur_root = queue.popleft()
+            if cur_root.left:
+                queue.append(cur_root.left)
+            if cur_root.right:
+                queue.append(cur_root.right)
+    return depth
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(6)
+root.right.right.left = TreeNode(8)
+root.right.left.right = TreeNode(7)
+print("The max depth is:", maxDepth(root))
